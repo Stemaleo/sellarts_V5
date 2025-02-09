d5151402427bd16b126bdf8025b1130072c8b613
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-02-09 22:28:54 +0000

    update ts

--- sell-arts-ui/src/app/[locale]/artist_app/tickets/[ticketId]/page.tsx
+++ sell-arts-ui/src/app/[locale]/artist_app/tickets/[ticketId]/page.tsx
@@ -3,3 +3,4 @@ import TicketConversation from "@/components/TicketConversation";
 export default async function TicketPage({ params }: { params: { ticketId: string } }) {
   return <TicketConversation ticketId={params.ticketId} />;
 }
+
