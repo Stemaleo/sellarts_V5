3c17897202317cfdc45648aa370d2d0bd076e51b
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-02-17 23:48:56 +0000

    update dockerfile

--- sell-arts-ui/cypress.config.ts
+++ sell-arts-ui/cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "cypress";
-import { NEXTAUTH_URL } from "./secret";
-export default defineConfig({
-  e2e: {
-    baseUrl: NEXTAUTH_URL,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+// import { defineConfig } from "cypress";
+// import { NEXTAUTH_URL } from "./secret";
+// export default defineConfig({
+//   e2e: {
+//     baseUrl: NEXTAUTH_URL,
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
