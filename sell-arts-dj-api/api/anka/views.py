7d06df6d049bbfeaf44e26fb98885342f2e65423
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-02-19 02:02:15 +0000

    add order to the amount

--- sell-arts-dj-api/api/anka/views.py
+++ sell-arts-dj-api/api/anka/views.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 def instant_payment_notification(request):
     """Endpoint for handling instant payment notifications (IPN)."""
     
-    logger.warning(request)
-    logger.warning(request.__dict__)
+    # logger.warning(request)
+    # logger.warning(request.__dict__)
     
     # request.post
     # data = request.__dict__['data']
@@ -21,21 +21,20 @@ def instant_payment_notification(request):
     #     anka_models.Orders
         
 
-    if request.method != "POST":
-        data = json.loads(request.body)
-        logger.warning(data)
+    if request.method == "POST":
+   
+        try:
+            data = json.loads(request.body.decode("utf-8"))['data']  # Convertit le corps en JSON
+            logger.warning("Received Instant Payment Notification: %s", data)
+             
+            return JsonResponse({"success": True, "message": "Notification received"}, status=200)
+
+        except json.JSONDecodeError:
+            logger.error("Invalid JSON received in IPN")
+            return JsonResponse({"success": False, "message": "Invalid JSON"}, status=400)
+
+        except Exception as e:
+            logger.exception("Error processing IPN")
+            return JsonResponse({"success": False, "message": "Internal Server Error"}, status=500)
+    else:
         return JsonResponse({"success": False, "message": "Invalid request method"}, status=405)
-
-    try:
-        data = json.loads(request.body.decode("utf-8"))  # Convertit le corps en JSON
-        logger.warning("Received Instant Payment Notification: %s", data)
-
-        return JsonResponse({"success": True, "message": "Notification received"}, status=200)
-
-    except json.JSONDecodeError:
-        logger.error("Invalid JSON received in IPN")
-        return JsonResponse({"success": False, "message": "Invalid JSON"}, status=400)
-
-    except Exception as e:
-        logger.exception("Error processing IPN")
-        return JsonResponse({"success": False, "message": "Internal Server Error"}, status=500)
