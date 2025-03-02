b9f6b928d379345236494582f95c65e730e50a6c
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-03-02 19:51:45 +0000

    add currency to initiate payment

--- sell-arts-dj-api/api/api/settings.py
+++ sell-arts-dj-api/api/api/settings.py
@@ -65,10 +65,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "api.urls"
 
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],  # Vérifie ce chemin
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
