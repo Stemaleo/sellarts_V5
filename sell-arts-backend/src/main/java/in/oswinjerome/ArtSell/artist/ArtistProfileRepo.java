3b1cc1dd72b1f69faa3629f02ca55840b09ccfff
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-03-05 23:48:13 +0000

    user deletion

--- sell-arts-backend/src/main/java/in/oswinjerome/ArtSell/artist/ArtistProfileRepo.java
+++ sell-arts-backend/src/main/java/in/oswinjerome/ArtSell/artist/ArtistProfileRepo.java
@@ -13,44 +13,40 @@ import java.util.Optional;
 
 public interface ArtistProfileRepo extends JpaRepository<ArtistProfile, Long> {
 
-    @Query(value = "SELECT * FROM artist_profiles WHERE is_deleted = false ORDER BY RANDOM() LIMIT 5", nativeQuery = true)
+    @Query("SELECT e FROM artist_profiles e  WHERE e.is_deleted=false ORDER BY random() LIMIT 5")
     List<ArtistProfile> findRandomRecords();
 
-    @Query("SELECT new in.oswinjerome.ArtSell.artist.ArtistProfileDTO(ap, COUNT(DISTINCT aw), COUNT(DISTINCT oi)) " +
-            "FROM ArtistProfile ap " +
+
+//    @Query("SELECT new in.oswinjerome.ArtSell.artist.ArtistProfileDTO(e) FROM artist_profiles e ")
+    @Query("SELECT new in.oswinjerome.ArtSell.artist.ArtistProfileDTO(ap, COUNT(DISTINCT aw),COUNT( DISTINCT oi)) " +
+            "FROM artist_profiles ap " +
             "LEFT JOIN ap.user u " +
-            "LEFT JOIN ArtWork aw ON aw.owner = u " +
-            "LEFT JOIN OrderItem oi ON oi.artWork = aw " +
-            "WHERE ap.artistType = :type AND (:name IS NULL OR u.name LIKE %:name%) AND ap.isDeleted = false AND u.isDeleted = false " +
+            "LEFT JOIN art_works aw ON aw.owner = u " +
+            "LEFT JOIN OrderItem  oi ON oi.artWork = aw "+
+            "WHERE ap.artistType = :type AND (:name IS NULL OR u.name LIKE %:name%) AND ap.is_deleted = false AND u.is_deleted=false " +
             "GROUP BY ap")
     Page<ArtistProfileDTO> getAllWithStats(ArtistType type, String name, Pageable pagable);
 
-    @Query("SELECT new in.oswinjerome.ArtSell.artist.ArtistProfileDTO(ap, COUNT(DISTINCT aw), COUNT(DISTINCT oi)) " +
-            "FROM ArtistProfile ap " +
-            "LEFT JOIN ap.user u " +
-            "LEFT JOIN ArtWork aw ON aw.owner = u " +
-            "LEFT JOIN OrderItem oi ON oi.artWork = aw " +
-            "WHERE ap.id = :artistId AND ap.isDeleted = false AND u.isDeleted = false " +
-            "GROUP BY ap")
-    Optional<ArtistProfileDTO> getArtistWithStats(Long artistId);
 
-    @Query("SELECT new in.oswinjerome.ArtSell.website.FeaturedArtistDTO(ap, aw) " +
-            "FROM ArtistProfile ap " +
-            "JOIN ArtWork aw ON aw.owner = ap.user " +
-            "WHERE ap.isDeleted = false AND ap.user.isDeleted = false " +
-            "GROUP BY ap, aw " +
-            "ORDER BY FUNCTION('RANDOM') " +
-            "LIMIT 5")
+    @Query("SELECT new in.oswinjerome.ArtSell.artist.ArtistProfileDTO(ap, COUNT(DISTINCT aw),COUNT( DISTINCT oi)) " +
+            "FROM artist_profiles ap " +
+            "LEFT JOIN ap.user u " +
+            "LEFT JOIN art_works aw ON aw.owner = u " +
+            "LEFT JOIN OrderItem  oi ON oi.artWork = aw "+
+            "where ap.id = :artistId AND ap.is_deleted = false AND u.is_deleted=false "+
+            "GROUP BY ap")
+   Optional< ArtistProfileDTO> getArtistWithStats(Long artistId);
+
+
+    @Query("SELECT new in.oswinjerome.ArtSell.website.FeaturedArtistDTO(e, a) FROM artist_profiles e " +
+            "JOIN art_works a ON a.owner = e.user " +
+            "GROUP BY e, a ORDER BY random() LIMIT 5")
     List<FeaturedArtistDTO> findRandomArtistWithArtWorks();
 
-    @Query("SELECT new in.oswinjerome.ArtSell.website.FeaturedArtistDTO(ap, aw) " +
-            "FROM ArtistProfile ap " +
-            "JOIN ArtWork aw ON aw.owner = ap.user " +
-            "WHERE ap.artistType = in.oswinjerome.ArtSell.artist.ArtistType.GALLERY " +
-            "AND ap.isDeleted = false AND ap.user.isDeleted = false " +
-            "GROUP BY ap, aw " +
-            "ORDER BY FUNCTION('RANDOM') " +
-            "LIMIT 3")
+    @Query("SELECT new in.oswinjerome.ArtSell.website.FeaturedArtistDTO(e, a) FROM artist_profiles e " +
+            "JOIN art_works a ON a.owner = e.user AND e.artistType = in.oswinjerome.ArtSell.artist.ArtistType.GALLERY " +
+            "GROUP BY e, a ORDER BY random() LIMIT 3")
     List<FeaturedArtistDTO> findRandomGalleryWithArtWorks();
 
 }
+
