48a3a209dc7172784e0c5f73e976a9fb9c86be00
Emon Nathan (Satoshi) <nathanuchia@gmail.com>
2025-03-20 12:09:11 +0000

    unable mailing

--- sell-arts-ui/src/app/[locale]/(auth)/register/page.tsx
+++ sell-arts-ui/src/app/[locale]/(auth)/register/page.tsx
@@ -37,7 +37,7 @@ export default function Component() {
   const t = useTranslations("register");
   const [country, setCountry] = useState<CountryType[]>([]);
   const [error, setError] = useState<string | null>(null);
-
+  const [selectedType, setSelectedType] = useState<string | undefined>('customer');
   const fetchCountry = async () => {
     try {
       const response = await axios.post(process.env.NEXT_PUBLIC_DJ_API_URL || "", {
@@ -99,34 +99,41 @@ export default function Component() {
       type: "customer",
     },
     validationSchema: Yup.object({
-      password: Yup.string().min(6, "Min 6 characters required").required("Required"),
+      password: Yup.string().required("Required"),
       confirmPassword: Yup.string().oneOf([Yup.ref("password")], "Passwords must match"),
-      country: Yup.string().required("Required"),
+      country: selectedType === "customer" ? Yup.string() : Yup.string().required("Required"),
       email: Yup.string().email("Invalid email address").required("Required"),
       name: Yup.string().required("Required"),
       type: Yup.string().required("Required"),
     }),
     async onSubmit(values, formikHelpers) {
       const res = await execute(register, values);
-      if (selectedType!=='customer') {
-        if (res?.success) {
-          const userId = (res?.data as resData).id
-          handleUserIdAndCountry(userId , values.country, values);
- 
-       
-      }
-      if (!res?.success) {
-        toast.error(res?.message);
+      if (res?.success) {
+        if (selectedType!=='customer') {
+            const userId = (res?.data as resData).id
+            handleUserIdAndCountry(userId , values.country, values);
+          }
+        if (selectedType==='customer') {
+          toast.success(t("loggingIn") + "...");
+          const res = await signIn("credentials", {
+            redirect: true,
+            callbackUrl: values.type == "artist" || values.type == "gallery" ? "/artist_app/profile" : "/",
+            ...values,
+          });
         }
       }
+
+      if (!res?.success) {
+          toast.error(res?.message);
+      }
     },
-  });
+  });   
   const userTypes = [
     { value: "customer", label: t("customer"), icon: User, description: t("i-want-to-browse-and-purchase-art") },
     { value: "artist", label: t("artist"), icon: Palette, description: t("i-want-to-showcase-and-sell-my-artwork") },
     { value: "gallery", label: t("gallery"), icon: Building2, description: t("i-represent-a-gallery-and-want-to-manage-exhibitions") },
   ];
-  const [selectedType, setSelectedType] = useState<string | undefined>('customer');
+
   const [showFields, setShowFields] = useState(false);
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-primary to-red-500">
